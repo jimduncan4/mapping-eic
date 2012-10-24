f05de05e45a0d0cbb12ae7dbed4c23fb85697b54
Jim Duncan <jimduncan4@gmail.com>
2012-10-24 11:09:12 -0300

    Adding infowindow filter to SPA and forest locations attribs

--- js/extractives.js
+++ js/extractives.js
@@ -33,12 +33,12 @@
 
  "Special Protected Areas": {
    infoWindowFilter:{"title": "$[placenamee] Special Protected Area","subtitle":"","tabs": [{"title": "<b>Data</b>","type": "text","value": "Special protected area boundaries provided by the Environmental Information Center. See the About page for more information"}]},
-   "Location":  {"type": "PRIMITIVE","stroke": {"color": [0x238443], "weight": 1, "opacity": 0.75},"fill":{"color":[0x238443],"opacity": 0.75}},
+   "Location":  {"infoWindowFilter": {"subtitle": ""},"type": "PRIMITIVE","stroke": {"color": [0x238443], "weight": 1, "opacity": 0.75},"fill":{"color":[0x238443],"opacity": 0.75}},
   },
 
 "Forest": {
    infoWindowFilter:{"title": "Forested Area","subtitle":"","tabs": [{"title": "<b>About</b>","type": "text","value": "Coverage of forest area in Mongolia. Data provided by the Environmental Information Center."}]},
-   "Location":  {"type": "PRIMITIVE","stroke": {"color": [0x005A32], "weight": 1, "opacity": 0.75},"fill":{"color":[0x005A32],"opacity": 0.75}},
+   "Location":  {"infoWindowFilter": {"subtitle": ""},"type": "PRIMITIVE","stroke": {"color": [0x005A32], "weight": 1, "opacity": 0.75},"fill":{"color":[0x005A32],"opacity": 0.75}},
   },
 
 "Aimag indicators": {}
